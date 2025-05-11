ea11c11e887d7ac0b188e462ba7507eb71b028d3
anailton <jesusanailton706@gmail.com>
2025-05-11 20:49:55 -0300

    calculadora de media

---
+++ 01-media/app.js
@@ -0,0 +1,17 @@
+function calcular(){
+    let ="n1, n2 n3, media"
+
+    n1 = document.getElementById("nota_1").value;
+    n2 = document.getElementById("nota_2").value;
+    n3 = document.getElementById("nota_3").value;
+
+    media = (parseFloat(n1) + parseFloat(n2) + parseFloat(n3)) / 3
+
+    document.getElementById("media").innerHTML = media
+
+    if(media < 7){
+        document.getElementById("status").innerHTML = "reprovado"
+    }else{
+        document.getElementById("status").innerHTML = "aprovado"
+    }
+}
